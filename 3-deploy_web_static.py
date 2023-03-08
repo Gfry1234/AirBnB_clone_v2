af004f3f9bf740650bbad40209b39798af7c521a
Adeniyi <adeniyi.oluwajana@gmail.com>
2023-03-08 16:06:01 +0000

    update3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,68 +1,75 @@
 #!/usr/bin/python3
-'''
-fabric script to distribute an archive to web servers
-----NEEDS TO REVISIT SCRIPT
-'''
+"""
+Fabric script (based on the file 2-do_deploy_web_static.py)
+that creates and distributes an archive to your web servers
+"""
+from fabric.api import *
+from os import path
+import time
 
-import os
-from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
+env.hosts = ["18.207.140.70", "100.24.236.57"]
+env.user = "ubuntu"
 
 
-env.hosts = ['18.207.140.70', '100.24.236.57']
-
-
-def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
+def do_pack():
     """
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
+    generates a .tgz archive from
+    the contents of the web_static folder
+    """
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+        local("mkdir -p versions")
+
+        t = time.strftime("%Y%m%d%H%M%S")
+        gen_path = "versions/web_static_{}.tgz".format(t)
+
+        local("tar -cvzf {} web_static".format(gen_path))
+
+        get_size = local('wc -c {}'.format(gen_path), capture=True)
+        get_size = get_size.split(" ")[0]
+
+        print("web_static packed: {} -> {}Bytes".format(gen_path, get_size))
+        return gen_path
     except Exception:
-        output = None
-    return output
+        return None
 
 
 def do_deploy(archive_path):
-    """Deploys the static files to the host servers.
-    Args:
-        archive_path (str): The path to the archived static files.
+    """ Deploy arcives to web servers
     """
-    if not os.path.exists(archive_path):
+    if not path.exists(archive_path):
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
-    try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
+
+    if path.exists(archive_path):
+        fn = archive_path.split("/")[-1].split(".")[0]
+        fn_ext = archive_path.split("/")[-1]
+        dwr = "/data/web_static/releases"
+
+        put(archive_path, "/tmp/{}".format(fn_ext))
+        run("mkdir -p /data/web_static/releases/{}".format(fn))
+
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(fn_ext, fn))
+
+        run("rm -f /tmp/{}".format(fn_ext))
+
+        run("mv {}/{}/web_static/* {}/{}/".format(dwr, fn, dwr, fn))
+
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(fn))
+
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version deployed!')
-        success = True
+        run("ln -s {}/{}/ /data/web_static/current".format(dwr, fn))
+        print("New version deployed!")
+        return True
+    else:
+        return False
+
+
+def deploy():
+    """Generate and deploy archives to web servers
+    """
+    try:
+        archive_path = do_pack()
+        return do_deploy(archive_path)
     except Exception:
-        success = False
-    return success
+        return False
